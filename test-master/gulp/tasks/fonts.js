6a8290b4fb037c3bf471b0805bedd4fcaf1519b3
lidiaapushkar <pushkar.sliceplanet@dmail.com>
2020-03-17 22:31:05 +0200

    Add belpsTest

--- test-master/gulp/tasks/fonts.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-const config = require('../config');
-
-gulp.task('fonts:build', (done) => {
-  gulp.src(config.dist.fonts.src).pipe(gulp.dest(config.dist.fonts.dest));
-  done();
-});
